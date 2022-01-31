f411461d74d938173d5411446da29c960ee4fa39
Max <djin85_85@mail.ru>
2022-02-01 00:36:39 +0300

    Add entity Player

---
+++ app/Models/Player.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['first_name', 'last_name', 'middle_name', 'number', 'team_id'];
+
+    public function team (): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
---
+++ database/migrations/2022_01_31_203955_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->integer('number')->nullable();
+            $table->bigInteger('team_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
