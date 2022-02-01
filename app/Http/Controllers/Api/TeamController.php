e70dd8aed211d4ebab2d294e53bcf41bbc66520e
Max <djin85_85@mail.ru>
2022-02-01 23:07:58 +0300

    Remove TeamResource and fix TeamController

--- app/Http/Controllers/Api/TeamController.php
+++ app/Http/Controllers/Api/TeamController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\TeamResource;
+use App\Models\Player;
+use App\Models\Staff;
 use Illuminate\Http\Request;
+use JetBrains\PhpStorm\ArrayShape;
 
 class TeamController extends Controller
 {
+    #[ArrayShape(['players' => "\App\Models\Player[]|\Illuminate\Database\Eloquent\Collection", 'stuff' => "\App\Models\Staff[]|\Illuminate\Database\Eloquent\Collection"])]
     public function list (): array
     {
-        //return TeamResource::collection(Team::paginate(20))->resolve();
-        return [];
+        return [
+            'players' => Player::all(),
+            'stuff' => Staff::all()
+        ];
     }
 }
