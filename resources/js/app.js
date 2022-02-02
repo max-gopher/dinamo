758b1d42f5d2dcabe3b03403f40ba724c34a5f28
darustill <rachikdarustill@gmail.com>
2022-02-02 22:57:13 +0300

    fix app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import App from '@/js/views/App.vue';
+import router from '@/js/router'
+
+import { $api } from '@/js/axios';
+
+Vue.config.productionTip = false;
+Vue.prototype.$api = $api;
+
+require('@/assets/sass/normalize.css')
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
