6e1f1349bc7978b881b394485e6dc994e3081f13
Max <djin85_85@mail.ru>
2022-02-23 16:08:55 +0300

    Fix add disc url to logo in TourResource.php

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class TourResource extends JsonResource
 {
@@ -15,7 +16,7 @@ class TourResource extends JsonResource
     public function toArray($request)
     {
         $result = [
-            'logo' => $this->club->logo,
+            'logo' => Storage::disk('clubs')->url($this->club->logo),
             'name' => $this->club->name,
             'results' => [
                 'games' => $this->games,
